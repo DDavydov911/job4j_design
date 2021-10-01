4c91dc422770424ab52bd098b9565c43f465454b
Denis Davydov <dvd9@yandex.ru>
2021-10-01 17:20:02 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -14,7 +14,11 @@ public class Search {
     }
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("java")).forEach(System.out::println);
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Call of file must have 2 arguments: root folder and extension. "
+                            + "Usage java -jar search.jar ROOT_FOLDER EXTENSION");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 }
