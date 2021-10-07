d458ccf9a0e975058edb74f944b5f272a1c034dd
Denis Davydov <dvd9@yandex.ru>
2021-10-07 12:11:33 +0300

    7. Scanner [#504791]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,52 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class CSVReader {
+    //    java -jar target/csvReader.jar -path=file.csv -delimiter=";"  -out=stdout -filter=name,age
+    public static void handle(ArgsName argsName) throws Exception {
+        String[] filters = argsName.get("filter").split(",");
+        String delim = argsName.get("delimiter");
+
+        Scanner scanner = new Scanner(new FileInputStream(argsName.get("path")));
+        ArrayList<ArrayList<String>> allLines = new ArrayList<>();
+        while (scanner.hasNext()) {
+            String nextLine = scanner.nextLine();
+            Scanner lineScanner = new Scanner(nextLine).useDelimiter(delim);
+            ArrayList<String> line = new ArrayList<>();
+            while (lineScanner.hasNext()) {
+                line.add(lineScanner.next());
+            }
+            allLines.add(line);
+            lineScanner.close();
+        }
+        scanner.close();
+
+        try (PrintStream printStream = new PrintStream(new FileOutputStream(argsName.get("out")))) {
+            for (ArrayList<String> allLine : allLines) {
+                StringJoiner sj = new StringJoiner(delim);
+                for (int j = 0; j < allLine.size(); j++) {
+                    for (String filter : filters) {
+                        if (filter.equals(allLines.get(0).get(j))) {
+                            sj.add(allLine.get(j));
+                        }
+                    }
+                }
+                printStream.println(sj);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ArgsName an = ArgsName.of(args);
+        CSVReader.handle(an);
+    }
+}
