b1a265b2e78dc49f12ab1155399b306637f8ee6d
Denis Davydov <dvd9@yandex.ru>
2021-10-08 17:27:57 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,18 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Jhon Dow";
+        byte age = 33;
+        short smth = 32000;
+        int count = 12345678;
+        long pass = 1234567890987654321L;
+        float high = 1.98F;
+        double smthToo = 3.87654;
+        char code = 'G';
+        boolean approved = true;
+
+        LOG.debug("\nUser info name : {},\n age : {},\n smth : {},\n count : {},\n"
+                + " pass : {},\n high : {},\n smthToo : {},\n cpde : {},\n approved : {}\n",
+                name, age, smth, count, pass, high, smthToo, code, approved);
     }
 }
